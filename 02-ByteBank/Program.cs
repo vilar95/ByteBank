3191f0cdfde4db26d5e148434ddcc21825fc2a40
Eduardo Vilar <e.vilar95@gmail.com>
2022-01-31 10:54:55 -0300

    Adicionar arquivos de projeto.

---
+++ 02-ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = "Gabriela";
+            conta.saldo = 200;
+
+            Console.WriteLine(conta.titular);
+            Console.WriteLine(conta.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}

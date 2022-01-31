7e92afdde61141b9adfdbebdd4da848161b5767e
Eduardo Vilar <e.vilar95@gmail.com>
2022-01-31 11:12:15 -0300

    Exercício de saldo.

--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -26,9 +26,28 @@ namespace _01_ByteBank
             contaDaGabriela.saldo += 200;
             Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
 
-            Console.ReadLine();
+          
 
+            
+            {
+                ContaCorrente primeiraContaCorrente = new ContaCorrente();
+                primeiraContaCorrente.saldo = 200;
+                Console.WriteLine(primeiraContaCorrente.saldo);
 
+                primeiraContaCorrente.saldo += 100;
+                Console.WriteLine(primeiraContaCorrente.saldo);
+
+                ContaCorrente segundaContaCorrente = new ContaCorrente();
+                segundaContaCorrente.saldo = 50;
+
+                Console.WriteLine("primeira conta tem " + primeiraContaCorrente.saldo);
+                Console.WriteLine("segunda conta tem " + segundaContaCorrente.saldo);
+                
+                Console.ReadLine();
+            }
         }
+
+
     }
-}
+    }
+

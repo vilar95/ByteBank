7a2bc6e421a8be1140fd8e6ef757da4fa75b83f8
EDUARDO VILAR <e.vilar95@gmail.com>
2022-02-07 11:57:03 -0300

    ByteBank2.0

---
+++ ByteBank2/ByteBank2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank2
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                ContaCorrente conta = new ContaCorrente(456, 0);
+            }
+            catch (ArgumentException ex)
+            {
+                if (ex.ParamName == "numero")
+                {
+
+                }
+
+                Console.WriteLine("Argumento com problema: " + ex.ParamName);
+                Console.WriteLine("Ocorreu uma exceção do tipo ArgumentExeption");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            Metodo();
+            
+            Console.WriteLine("Execção finalizada. Tecle enter para sair");
+            Console.ReadLine();
+        }
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }  
+    }
+}
